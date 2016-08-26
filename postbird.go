f9e97d7b7eac1e8442529acd8325476768be6122
SeongUk Cho <issacs98@gmail.com>
2016-08-27 02:22:37 +0900

    some changes

--- postbird.go
+++ postbird.go
@@ -31,6 +31,9 @@ type Client struct {
 	ClientID   string
 }
 
+type CallEvent struct {
+}
+
 const DefaultPort uint = 8787                   // Default Bind Port
 const DefaultBindAddress string = "127.0.0.1"   // Default Bind Address
 const DefaultRemoteAddress string = "127.0.0.1" // Defualt Server Address
@@ -170,7 +173,7 @@ func Listener(BindAddr string, Port uint) {
 
 	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("./asset")))
-	http.ListenAndServe(BindAddr, nil)
+	http.ListenAndServe(BindAddr+":"+string(Port), nil)
 }
 
 // Binder func
@@ -189,6 +192,9 @@ func Binder(BindAddr string, Port uint) {
 		}
 		defer conn.Close()
 
+		ClientId := RandStringRunes(17)
+		Clients = append(Clients, Client{nil, conn, ClientId})
+
 		go requestHandler(conn)
 	}
 }
@@ -196,23 +202,7 @@ func Binder(BindAddr string, Port uint) {
 // requestHandler func
 // tcp 연결되었을때 request 핸들러
 func requestHandler(c net.Conn) {
-	data := make([]byte, 4096) // 4096 크기의 바이트 슬라이스 생성
-
-	for {
-		n, err := c.Read(data) // 클라이언트에서 받은 데이터를 읽음
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println(string(data[:n])) // 데이터 출력
-
-		_, err = c.Write(data[:n]) // 클라이언트로 데이터를 보냄
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
+	readFully(c)
 }
 
 func ConnectToRemote() {
